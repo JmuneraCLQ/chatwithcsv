7b22619e599bcc1d3361b529ac650860fe273de4
JmuneraCLQ <jmunera@clq.com.co>
2024-07-30 10:47:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from langchain.agents.agent_types import AgentType
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
 from langchain_openai import ChatOpenAI
+
 # Setting up the api key
 import environ
 
